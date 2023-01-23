677beeb4ca175c7d95359928277fa58f41e4fc9a
liesegang <yyama0704@gmail.com>
2023-01-24 00:03:46 +0900

    Add panzoom to MainCanvas

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import ImageItem from "./ImageItem.vue";
+import createPanZoom, { PanZoomOptions } from "panzoom";
+import { ref, onMounted, Ref } from "vue";
+
+let options: PanZoomOptions = {
+  autocenter: true,
+  bounds: true,
+  transformOrigin: {
+    x: 0.5,
+    y: 0.5,
+  },
+};
+const canvas: Ref<HTMLElement | undefined> = ref(undefined);
+
+onMounted(() => {
+  createPanZoom(canvas.value!, options);
+});
 </script>
 
 <template>
-  <div id="main-canvas">
+  <div id="main-canvas" ref="canvas">
     <ImageItem />
   </div>
 </template>
