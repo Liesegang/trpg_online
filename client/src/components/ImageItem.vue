6d9d3b309a6a6169bf1b36b1960095df15b4d6fd
liesegang <yyama0704@gmail.com>
2023-01-25 23:08:08 +0900

    Move client to client folder

---
+++ client/src/components/ImageItem.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { defineProps, onMounted, Ref, ref } from "vue";
+
+const props = defineProps({
+  zoom: {
+    type: Number,
+    default: 1,
+  }
+});
+
+interface Position {
+  top: number;
+  left: number;
+}
+
+const position: Ref<Position> = ref({ top: 0, left: 0 });
+const prevX: Ref<number> = ref(0);
+const prevY: Ref<number> = ref(0);
+const isDragging: Ref<boolean> = ref(false);
+
+onMounted(() => {
+  document.addEventListener("pointermove", handleDoDrag.bind(this));
+  document.addEventListener("mouseup", handleStopDrag.bind(this));
+});
+
+const handleStartDrag = (e: MouseEvent) => {
+  document.body.style.userSelect = 'none';
+  isDragging.value = true;
+  prevY.value = e.clientY;
+  prevX.value = e.clientX;
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function handleStopDrag(e: MouseEvent) {
+  document.body.style.userSelect = 'auto';
+  isDragging.value = false;
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function handleDoDrag(e: MouseEvent) {
+  if (!isDragging.value) {
+    return;
+  }
+  const diffY: number = e.clientY - prevY.value;
+  const diffX: number = e.clientX - prevX.value;
+  prevY.value = e.clientY;
+  prevX.value = e.clientX;
+  position.value.top += diffY / props.zoom;
+  position.value.left += diffX / props.zoom;
+  e.stopPropagation();
+  e.preventDefault();
+}
+</script>
+
+<template>
+  <img
+    src="@/assets/logo.png"
+    :style="{ top: `${position.top}px`, left: `${position.left}px` }"
+    @mousedown="handleStartDrag"
+  />
+</template>
+
+<style scoped>
+img {
+  position: absolute;
+}
+</style>
--- client/src/components/MainCanvas.vue
+++ client/src/components/MainCanvas.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ImageItem from "./ImageItem.vue";
-import createPanZoom, { PanZoomOptions } from "panzoom";
+import createPanZoom, { PanZoom, PanZoomOptions } from "panzoom";
 import { ref, onMounted, Ref } from "vue";
 
 let options: PanZoomOptions = {
@@ -10,17 +10,22 @@ let options: PanZoomOptions = {
     x: 0.5,
     y: 0.5,
   },
+  maxZoom: 10,
+  minZoom: 0.1,
+  smoothScroll: false
 };
 const canvas: Ref<HTMLElement | undefined> = ref(undefined);
+const instance: Ref<PanZoom | undefined> = ref(undefined);
 
 onMounted(() => {
-  createPanZoom(canvas.value!, options);
+  instance.value = createPanZoom(canvas.value!, options);
 });
 </script>
 
 <template>
   <div id="main-canvas" ref="canvas">
-    <ImageItem />
+    <ImageItem :zoom="instance?.getTransform().scale"/>
+    <ImageItem :zoom="instance?.getTransform().scale"/>
   </div>
 </template>
 
