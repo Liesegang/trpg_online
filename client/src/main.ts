de8cb7b45f5ea816532ce6c9bd3363481f673333
liesegang <yyama0704@gmail.com>
2023-01-26 22:57:49 +0900

    Add axios settings

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import axios from 'axios'
 import App from "./App.vue";
 
 import "@mdi/font/css/materialdesignicons.css";
@@ -6,6 +7,10 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
+axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 // eslint-disable-next-line @typescript-eslint/typedef
 const vuetify = createVuetify({
