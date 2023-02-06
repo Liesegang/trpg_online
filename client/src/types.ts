f867cb3bb71e7734fc69b0ef3eebaddfa325ec18
liesegang <yyama0704@gmail.com>
2023-02-06 23:51:21 +0900

    Add properties to Character class

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,6 +1,22 @@
+export interface Parameter {
+  name: string;
+  value: number;
+}
+
+export interface Status {
+  name: string;
+  value: number;
+}
+export interface Skill {
+  name: string;
+  value: number;
+}
 
 export interface Character {
   id: string;
   name: string;
   transform: string;
+  parameter: Parameter[];
+  status: Status[];
+  skill: Skill[];
 }
